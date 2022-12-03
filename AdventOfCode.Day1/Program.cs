58554194aa94227e2acef78ad6667ef3a7faaa4e
Patrick Koning <patrick@pkoning.nl>
2022-12-03 19:22:32 +0100

    + Solution for day 1.

---
+++ AdventOfCode.Day1/Program.cs
@@ -0,0 +1,19 @@
+﻿using Spectre.Console;
+
+namespace AdventOfCode.Day1;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var path = AnsiConsole.Ask<string>("What is the path to the input?");
+
+        var parser = new Parser(path);
+
+        var highestCalories = await parser.GetHighestCaloriesAsync();
+
+        AnsiConsole.MarkupLineInterpolated($"Highest calories: {highestCalories}");
+    }
+
+    
+}
