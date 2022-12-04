210f90061ec794e6044c51d389363a88884397ed
Patrick Koning <pkoning@mypoiworld.com>
2022-12-04 22:53:24 +0100

    + Solution for day 3 part 2.

--- AdventOfCode.Day1/Day3/ItemTypeExtensions.cs
+++ AdventOfCode.Day1/Day3/ItemTypeExtensions.cs
@@ -1,16 +1,10 @@
 ﻿namespace AdventOfCode.Day3
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-
     internal static class ItemTypeExtensions
     {
         internal static int ToItemTypePriority(this char itemType)
         {
-            var charValue = (int)itemType;
+            int charValue = (int)itemType;
             if (charValue is >= 97 and <= 122)
             {
                 return charValue - 96;
--- AdventOfCode.Day1/Day3/RucksackSolver.cs
+++ AdventOfCode.Day1/Day3/RucksackSolver.cs
@@ -5,22 +5,29 @@
         async Task<string> IPuzzleSolver.SolvePuzzleAsync(string inputPath)
         {
             int totalPriorities = 0;
+
+            var index = 0;
+            IEnumerable<char> intersect = Array.Empty<char>();
             await foreach (string line in File.ReadLinesAsync(inputPath))
             {
-                IEnumerable<char> sameItemTypes = GetSameItemTypes(line);
-                totalPriorities += sameItemTypes.Sum(itemType => itemType.ToItemTypePriority());
+                var itemTypes = line.ToCharArray();
+                if (index % 3 == 0)
+                {
+                    totalPriorities += intersect.Sum(itemType => itemType.ToItemTypePriority());
+
+                    intersect = itemTypes;
+                }
+                else
+                {
+                    intersect = intersect.Intersect(itemTypes);
+                }
+
+                index++;
             }
 
+            totalPriorities += intersect.Sum(itemType => itemType.ToItemTypePriority());
+
             return totalPriorities.ToString();
-        }
-
-        private static IEnumerable<char> GetSameItemTypes(string rucksack)
-        {
-            int halfMark = rucksack.Length / 2;
-            string firstPart = rucksack[..halfMark];
-            string lastPart = rucksack[halfMark..];
-
-            return firstPart.ToCharArray().Intersect(lastPart.ToCharArray()).ToList();
         }
     }
 }
